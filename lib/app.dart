a775cd8890c7e00d67909b52982456a3e3e3b369
Matias Meno <m@tias.me>
2016-10-06 11:33:32 +0200

    Broke version with code in remote file

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:angular2/core.dart';
+import 'package:angular2/platform/browser.dart';
+
+// ignore: UNUSED_IMPORT
+import 'package:google_maps/google_maps.dart';
+
+@Component(
+  selector: 'main-app',
+  template: 'foo',
+)
+class MainApp {}
+
+main() async {
+  bootstrap(MainApp);
+}
--- web/index.dart
+++ web/index.dart
@@ -1,15 +1,3 @@
-import 'package:angular2/core.dart';
-import 'package:angular2/platform/browser.dart';
+import 'package:angular_maps/app.dart' as app;
 
-// ignore: UNUSED_IMPORT
-import 'package:google_maps/google_maps.dart';
-
-@Component(
-  selector: 'main-app',
-  template: 'foo',
-)
-class MainApp {}
-
-main() {
-  bootstrap(MainApp);
-}
+main() => app.main();
